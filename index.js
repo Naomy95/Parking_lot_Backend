a3eb153f6b552c8854d7f04eedcf827eabd5dcdd
Naomy95 <zannatnaomy@gmail.com>
2023-05-14 21:25:55 +0600

    first vercel deploy

--- index.js
+++ index.js
@@ -9,6 +9,18 @@ const port=process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://parking-lot:zMRSytRoK1wKcGRr@cluster0.kvzsn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+client.connect(err=>{
+    const collection=client.db("parking_lot").collection("users")
+    console.log('connected')
+
+})
+
+// console.log(client)
+
 app.get('/', (req, res) => {        
     res.send("user");      
 });
